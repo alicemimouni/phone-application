b99b8aab26d5976ed94042ec51dce969cbfe9982
alicemimouni <alicerichardeau@outlook.com>
2023-11-16 17:36:56 +0100

    update files

--- src/main/java/com/alice/phoneBook/Contact.java
+++ src/main/java/com/alice/phoneBook/Contact.java
@@ -1,42 +1,54 @@
 package com.alice.phoneBook;
 
+// Class representing a contact in a phone book
 public class Contact {
-	
-	private String lastName;
-	private String firstName;
-	
-	private String phoneNumber;
-	
-	public Contact(String lastName, String firstName, String phoneNumber) {
-		this.lastName 	 = lastName;
-		this.firstName 	 = firstName;
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    
+    // Fields to store contact information
+    private String lastName;
+    private String firstName;
+    private String phoneNumber;
 
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    // Constructor to initialize a contact with provided information
+    public Contact(String lastName, String firstName, String phoneNumber) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.phoneNumber = phoneNumber;
+    }
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    // Getter method for retrieving the last name
+    public String getLastName() {
+        return lastName;
+    }
 
-	@Override
-	public String toString() {
-		return this.lastName + " " + this.firstName + " " + this.phoneNumber;
-	}
+    // Setter method for updating the last name
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
+    // Getter method for retrieving the first name
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // Setter method for updating the first name
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter method for retrieving the phone number
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setter method for updating the phone number
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Override toString method to represent the contact as a string
+    @Override
+    public String toString() {
+        // Format the contact information as "Last Name First Name Phone Number"
+        return this.lastName + " " + this.firstName + " " + this.phoneNumber;
+    }
 }
--- src/main/java/com/alice/phoneBook/PhoneBook.java
+++ src/main/java/com/alice/phoneBook/PhoneBook.java
@@ -7,62 +7,77 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class PhoneBook {
-	
-	public static final String PHONE_BOOK_FILE_PATH = "C:\\Users\\alice\\eclipse-workspace\\phone-application\\phonebook.txt";
-	public static Scanner sc = null;
 
-	public static void main(String[] args) {
-		
-		sc = new Scanner(System.in);
-		
-		String userLastName 	= getUserInput("Entrez un nom de famille: ");
-		String userFirstName  	= getUserInput("Entrez un prénom: ");
-		String userPhoneNumber 	= getUserInput("Entrez un numéro de téléphone: ");
-		
-		Contact newContact  = new Contact(userLastName, userFirstName, userPhoneNumber);
-		
-		File phoneBookFile = getOrCreatePhoneBookFile(PHONE_BOOK_FILE_PATH);
-		appendContactToPhoneBook(phoneBookFile, newContact);
-		
-		sc.close();
-	}
-	
-	public static String getUserInput(String userRequest) {
-		System.out.println(userRequest);
-		return sc.nextLine();
-	}
-	
-	public static File getOrCreatePhoneBookFile(String phoneBookFilePath) {
-		
-		File phoneBookFile = new File(phoneBookFilePath);
-		
-		if(phoneBookFile.exists()) {
-			return phoneBookFile;
-		}
-		
-		try {
-			phoneBookFile.createNewFile();
-			System.out.println("Le fichier a été créé (" + phoneBookFilePath + ")");
-			return phoneBookFile;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return null;
-	}
-	
-	//	add contact to phone book file
-	public static void appendContactToPhoneBook(File phoneBookFile, Contact newContact) {
-		
-		try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true))) {
-			
-			fileWriter.append(newContact.toString());
-			fileWriter.append(System.lineSeparator());
-			
-			System.out.println("Contact ajouté");
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    // File path for the phone book file
+    public static final String PHONE_BOOK_FILE_PATH = "C:\\Users\\alice\\eclipse-workspace\\phone-application\\phonebook.txt";
+    // Scanner for user input
+    public static Scanner sc = null;
+
+    public static void main(String[] args) {
+
+        // Initialize the scanner
+        sc = new Scanner(System.in);
+
+        // Get user inputs for contact information
+        String userLastName = getUserInput("Entrez un nom de famille : ");
+        String userFirstName = getUserInput("Entrez un prénom : ");
+        String userPhoneNumber = getUserInput("Entrez un numéro de téléphone : ");
+
+        // Create a new Contact object with the user inputs
+        Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
+
+        // Get or create the phone book file
+        File phoneBookFile = getOrCreatePhoneBookFile(PHONE_BOOK_FILE_PATH);
+        // Append the new contact to the phone book file
+        appendContactToPhoneBook(phoneBookFile, newContact);
+
+        // Close the scanner
+        sc.close();
+    }
+
+    // Method to get user input
+    //nextLine is a method in the Scanner class in Java, commonly used for reading a line of input from the user
+    public static String getUserInput(String userRequest) {
+
+        System.out.println(userRequest);
+        return sc.nextLine(); 
+    }
+
+    // Method to get or create the phone book file
+    public static File getOrCreatePhoneBookFile(String phoneBookFilePath) {
+
+        File phoneBookFile = new File(phoneBookFilePath);
+
+        if (phoneBookFile.exists()) {
+            return phoneBookFile;
+        }
+
+        try {
+            // If the file doesn't exist, create a new one
+            phoneBookFile.createNewFile();
+            System.out.println("Le contact a bien été créé (" + phoneBookFilePath + ")");
+            return phoneBookFile;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // Method to add a contact to the phone book file
+    public static void appendContactToPhoneBook(File phoneBookFile, Contact newContact) {
+
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true))) {
+
+            // Append the contact information to the file
+            fileWriter.append(newContact.toString());
+            fileWriter.append(System.lineSeparator());
+
+            System.out.println("Contact ajouté");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
